b13a3e136cec8093c44127ca26baea23cf19492a
Annette Odingo <odingoannette@gmail.com>
2020-05-09 11:16:12 +0100

    Create an endpoint that return the countries alphabetically that have a name equal to or longer than the given length

--- src/main/java/com/lambdaschool/demo/CountryController.java
+++ src/main/java/com/lambdaschool/demo/CountryController.java
@@ -25,5 +25,15 @@ public class CountryController {
                 DemoApplication.ourCountryList.findCountries(e->e.getName().toUpperCase().charAt(0)==Character.toUpperCase(letter));
         return new ResponseEntity<>(rtnCountry, HttpStatus.OK);
     }
+//   localhost:2020/names/size/{number} - return the countries alphabetically that have a name equal to or longer than
+//   the given length
+    @GetMapping(value="/size/{number}", produces = "application/json")
+    public ResponseEntity<?> getCountriesLargeAndEqualToNumber(@PathVariable int number){
+        ArrayList<Country> rtnCountry =  DemoApplication.ourCountryList.findCountries(c->c.getLandMassKm2() >= number);
+        rtnCountry.sort((c1,c2)->c1.getName().compareToIgnoreCase(c2.getName()));
+        return new ResponseEntity<>( rtnCountry, HttpStatus.OK);
+    }
+
+
 
 }
