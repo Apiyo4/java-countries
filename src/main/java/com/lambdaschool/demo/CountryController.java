6de1275e5d877503404f36bc731fab8fa5e7d6b5
Annette Odingo <odingoannette@gmail.com>
2020-05-09 11:02:09 +0100

    Create an endpoint that retrieves all countries given a letter

--- src/main/java/com/lambdaschool/demo/CountryController.java
+++ src/main/java/com/lambdaschool/demo/CountryController.java
@@ -3,16 +3,27 @@ package com.lambdaschool.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
-@RequestMapping("/name")
+@RequestMapping("/names")
 public class CountryController {
-    //localhost:2020/name/all
+    //localhost:2020/names/all
     @GetMapping(value = "/all", produces = "application/json")
     public ResponseEntity<?> getAllCountries(){
         DemoApplication.ourCountryList.countryList.sort((c1,c2)->c1.getName().compareToIgnoreCase(c2.getName()));
         return  new ResponseEntity<>(DemoApplication.ourCountryList.countryList, HttpStatus.OK);
     }
+//  localhost:2020/names/start/{letter}
+    @GetMapping(value="start/{letter}", produces = "application/json")
+    public ResponseEntity<?> getCountyByLetter(@PathVariable char letter){
+        ArrayList<Country> rtnCountry =
+                DemoApplication.ourCountryList.findCountries(e->e.getName().toUpperCase().charAt(0)==Character.toUpperCase(letter));
+        return new ResponseEntity<>(rtnCountry, HttpStatus.OK);
+    }
+
 }
