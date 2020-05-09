18f6d6a656ad4c2933d87e105c26f007c0846f25
Annette Odingo <odingoannette@gmail.com>
2020-05-09 07:29:50 +0100

    Create an endpoint that retrieves all countries

---
+++ src/main/java/com/lambdaschool/demo/CountryController.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/name")
+public class CountryController {
+    //localhost:2020/name/all
+    @GetMapping(value = "/all", produces = "application/json")
+    public ResponseEntity<?> getAllCountries(){
+        DemoApplication.ourCountryList.countryList.sort((c1,c2)->c1.getName().compareToIgnoreCase(c2.getName()));
+        return  new ResponseEntity<>(DemoApplication.ourCountryList.countryList, HttpStatus.OK);
+    }
+}
--- src/main/java/com/lambdaschool/demo/DemoApplication.java
+++ src/main/java/com/lambdaschool/demo/DemoApplication.java
@@ -3,11 +3,16 @@ package com.lambdaschool.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class DemoApplication {
-
+    static CountryList ourCountryList;
     public static void main(String[] args) {
+        ourCountryList = new CountryList();
         SpringApplication.run(DemoApplication.class, args);
+
     }
+
 
 }
