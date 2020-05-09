2ee25cc794f5d6d75cbbf5f29558fcb333ad3cdb
Annette Odingo <odingoannette@gmail.com>
2020-05-09 07:01:29 +0100

    Generate setters and getters

--- src/main/java/com/lambdaschool/demo/Country.java
+++ src/main/java/com/lambdaschool/demo/Country.java
@@ -17,4 +17,44 @@ public class Country {
         this.landMassKm2 = landMassKm2;
         this.medianAge = medianAge;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public int getLandMassKm2() {
+        return landMassKm2;
+    }
+
+    public void setLandMassKm2(int landMassKm2) {
+        this.landMassKm2 = landMassKm2;
+    }
+
+    public int getMedianAge() {
+        return medianAge;
+    }
+
+    public void setMedianAge(int medianAge) {
+        this.medianAge = medianAge;
+    }
 }
